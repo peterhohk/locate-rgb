90b047d7a4bd45e508ed55f9bde18a696405502f
peterhohk <peterhohk0409@gmail.com>
2023-09-28 20:28:29 +0800

    attempt to use github actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './',
-  build: {
-    outDir: 'docs'
-  }
+  base: '/locate-rgb/'
 })
